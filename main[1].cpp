976cedd191b0310bae8b1870fb8012585571c290
Divyanshi-solanki <161625343+Divyanshi-solanki@users.noreply.github.com>
2024-09-07 16:19:00 +0530

    Add files via upload

---
+++ main[1].cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    /*
+           ( \" )is written either side of word or letter to be printed with " "
+           ( \' )is written either side of word or letter to be printed with ' '
+*/
+    cout<<"\"Hello World\"";
+    
+    cout<<"\'Hello World\'";
+
+    return 0;
+}
